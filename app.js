a1d008d276c6cb666b645729541b27d897d057be
Miki <miquelxubet@gmail.com>
2023-12-09 20:28:26 +0100

    conexió mongodb amb express commit

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8000
+
+require('dotenv').config();
+
+const mongoose = require("mongoose");
+const mongoDB = "mongodb+srv://"+process.env.DB_USER+":"+process.env.DB_PASSWORD+"@"+process.env.DB_SERVER+"/"+process.env.DB_NAME+"?retryWrites=true&w=majority";
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+main().catch(err => console.log(err));
 
 app.get('/', (req, res) => {
+  console.log(process.env.DB_USER)
   res.send('Hello World!')
 })
 
